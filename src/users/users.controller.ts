dfe7538ba9ea82d3d48716c79fc7778319ceff15
won-n-only <siliver12@gmail.com>
2024-06-22 12:35:09 +0900

    remove: get-user.dto
    _id 하나만 사용하므로 dto 삭제

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,8 +10,9 @@ import {
 } from '@nestjs/common'
 import { UsersService } from './users.service'
 import { JwtAuthRestGuard } from '../guards/jwt-auth.rest.guard'
-import { ReqGetUserDto } from './dto/request/get-user.dto'
 import { ResGetUserDto } from './dto/response/get-user.dto'
+import { Types } from 'mongoose'
+
 @UseGuards(JwtAuthRestGuard)
 @Controller('users')
 export class UsersController {
@@ -19,17 +20,17 @@ export class UsersController {
 
   @Get()
   @HttpCode(HttpStatus.OK)
-  async getUser(@Query() reqGetUserDto: ReqGetUserDto): Promise<ResGetUserDto> {
-    return this.usersService.findOne(reqGetUserDto)
+  async getUser(@Query() userid: Types.ObjectId): Promise<ResGetUserDto> {
+    return this.usersService.findOne(userid)
   }
 
   @Patch('/avatar')
   @HttpCode(HttpStatus.OK)
   async patchUserAvatar(
-    @Query() reqGetUserDto: ReqGetUserDto,
+    @Query() userid: Types.ObjectId,
     /**TODO: 아바타 obj 아니고 indexnumber면 나중에 바꿔야함 */
     @Body() avatar: Object,
   ): Promise<Object> {
-    return this.usersService.patchAvatar(reqGetUserDto, avatar)
+    return this.usersService.patchAvatar(userid, avatar)
   }
 }
