2db963c8571d3044d782bc39ef6b0aff72bf5613
won-n-only <siliver12@gmail.com>
2024-06-22 10:53:05 +0900

    update: return value added to Socket 'getFriends'

--- src/common/common.repository.ts
+++ src/common/common.repository.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
-import { Friend, User } from '../entities/user.entity'
-import { Model, Types } from 'mongoose'
+import { User } from '../entities/user.entity'
+import { Model, Types, ObjectId } from 'mongoose'
 
 @Injectable()
 export class CommonRepository {
@@ -9,7 +9,7 @@ export class CommonRepository {
     @InjectModel(User.name) private readonly userModel: Model<User>,
   ) {}
 
-  async getFriends(userId: Types.ObjectId) {
-    return await this.userModel.findById(userId, { friends: [Friend] })
+  async getFriends(userId: Types.ObjectId): Promise<ObjectId[]> {
+    return await this.userModel.findById(userId, { friends: 1 })
   }
 }
