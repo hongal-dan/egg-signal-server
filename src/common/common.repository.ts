ac77e93a3715e7c2f6cb6df4c3aedda50ce05797
won-n-only <siliver12@gmail.com>
2024-06-22 10:44:48 +0900

    feat: Socket '/common/getFriends'
    친구 목록 불러오기 구현

--- src/common/common.repository.ts
+++ src/common/common.repository.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
-import { User } from '../entities/user.entity'
-import { Model } from 'mongoose'
+import { Friend, User } from '../entities/user.entity'
+import { Model, Types } from 'mongoose'
 
 @Injectable()
 export class CommonRepository {
   constructor(
     @InjectModel(User.name) private readonly userModel: Model<User>,
   ) {}
+
+  async getFriends(userId: Types.ObjectId) {
+    return await this.userModel.findById(userId, { friends: [Friend] })
+  }
 }
