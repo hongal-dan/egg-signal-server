376582d91145c42acc40e6e707e29c8699a956e0
Amborsia <ghdrms1220@naver.com>
2024-07-05 16:34:33 +0900

    feat: jwt 모듈 설정 추가

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,33 @@
-import { Module } from '@nestjs/common'
+import { Module, forwardRef } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import { User, UserSchema } from '../entities/user.entity'
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
 import { AuthRepository } from './auth.repository'
-import { JwtService } from '@nestjs/jwt'
-
+import { JwtModule, JwtService } from '@nestjs/jwt'
+import { KakaoStrategy } from './oauth/oauth-kakao.strategy'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { PassportModule } from '@nestjs/passport'
+import { UsersModule } from '../users/users.module'
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: User.name, schema: UserSchema, collection: 'Users' },
     ]),
+    ConfigModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRES_IN') },
+      }),
+      inject: [ConfigService],
+    }),
+    forwardRef(() => UsersModule), // UsersModule 임포트
   ],
   controllers: [AuthController],
-  providers: [AuthService, AuthRepository, JwtService],
+  providers: [AuthService, AuthRepository, KakaoStrategy],
   exports: [
     MongooseModule.forFeature([
       { name: User.name, schema: UserSchema, collection: 'Users' },
