1f796bbe10c1ccef4b1f4d5bf425d8e2f8acfc22
Amborsia <ghdrms1220@naver.com>
2024-07-05 16:36:05 +0900

    feat: forwardRef를 통한 작업 및 MongooseModule을 사용하기 위한작업

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,14 +1,20 @@
-import { Module } from '@nestjs/common'
+import { Module, forwardRef } from '@nestjs/common'
 import { AuthModule } from '../auth/auth.module'
 import { UsersController } from './users.controller'
 import { UsersService } from './users.service'
 import { UsersRepository } from './users.repository'
 import { JwtAuthRestGuard } from '../guards/jwt-auth.rest.guard'
 import { JwtService } from '@nestjs/jwt'
+import { MongooseModule } from '@nestjs/mongoose'
+import { User, UserSchema } from '../entities/user.entity'
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    forwardRef(() => AuthModule),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository, JwtService, JwtAuthRestGuard],
+  exports: [UsersService],
 })
 export class UsersModule {}
