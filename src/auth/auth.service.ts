8d7fe48abb2e1dc0c3569631a7383e15b7038721
Amborsia <ghdrms1220@naver.com>
2024-07-05 16:38:49 +0900

    feat: 기존코드 에러핸들링 작업 및 import 작업

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,15 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common'
+import {
+  Injectable,
+  UnauthorizedException,
+  InternalServerErrorException,
+} from '@nestjs/common'
 import { AuthRepository } from './auth.repository'
 import { CreateUserDto } from './dto/request/create-user.dto'
 import { SignInUserDto } from './dto/request/signin-user.dto'
 import { JwtService } from '@nestjs/jwt'
 import * as bcrypt from 'bcrypt'
+import { KakaoUserDocument } from '../entities/user.entity'
+import { Types } from 'mongoose'
 
 @Injectable()
 export class AuthService {
@@ -13,33 +19,38 @@ export class AuthService {
   ) {}
 
   async create(createUserDto: CreateUserDto) {
-    const hashedCreateUserDto = await this.hashPassword(createUserDto)
-    const user = await this.authRepository.create(hashedCreateUserDto)
-
-    return user._id
+    try {
+      const hashedCreateUserDto = await this.hashPassword(createUserDto)
+      const user = await this.authRepository.create(hashedCreateUserDto)
+      return user._id
+    } catch (error) {
+      throw new InternalServerErrorException('Error creating user')
+    }
   }
 
   async signIn(signInUserDto: SignInUserDto) {
-    const user = await this.authRepository.findOne({ id: signInUserDto.id })
-    if (!user) {
-      throw new UnauthorizedException('Id not found')
-    }
+    try {
+      const user = await this.authRepository.findOne({ id: signInUserDto.id })
+      if (!user) {
+        throw new UnauthorizedException('Id not found')
+      }
 
-    const passwordMatch = await bcrypt.compare(
-      signInUserDto.password,
-      user.password,
-    )
+      const passwordMatch = await bcrypt.compare(
+        signInUserDto.password,
+        user.password,
+      )
 
-    if (!passwordMatch) {
-      throw new UnauthorizedException('Invalid credentials')
-    }
+      if (!passwordMatch) {
+        throw new UnauthorizedException('Invalid credentials')
+      }
 
-    const payload = { _id: user._id, id: user.id, nickname: user.nickname }
+      const payload = { _id: user._id, id: user.id, nickname: user.nickname }
 
-    return {
-      access_token: this.jwtService.sign(payload, {
-        secret: process.env.JWT_SECRET,
-      }),
+      return {
+        access_token: this.jwtService.sign(payload),
+      }
+    } catch (error) {
+      throw new InternalServerErrorException('Error signing in')
     }
   }
 
